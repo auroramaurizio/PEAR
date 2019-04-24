f19b9738ee1846496823088b424c2414509783d8
auroramaurizio <auroramaurizio1@gmail.com>
2019-04-24 15:58:15 +0200

    Delete __init__.py

--- pear/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = "Aurora Maurizio, Nicola Demo"
-__copyright__ = "Copyright 2019, GRAPE contributors"
-__version__ = "0.0.1"
-__mail__ = 'auroramaurizio1@gmail.com'
-__maintainer__ = __author__
-__status__ = "Alpha"
-
-from .general_graph import GeneralGraph
